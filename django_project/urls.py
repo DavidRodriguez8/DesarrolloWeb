95ae064af873c9e0dbadf663e11d1633fd26b835
David Rodriguez <davidlive.159@gmail.com>
2024-01-10 18:01:37 -0500

    Capitulo 6 Django

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include # new
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("pages.urls")), # new
+    # Django admin
+    path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")), # new
+    path("", include("pages.urls")),
 ]
